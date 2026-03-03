62e907fce44fb0a0662caa5529ec509a22a24ac2
sithnuwanpramudith <sithnuwanpramudith2000@gmail.com>
2026-03-03 12:48:28 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const app = express();
-//const port = 5000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
