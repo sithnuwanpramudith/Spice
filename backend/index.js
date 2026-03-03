94f9457876995d8a211a2aa984c80b084fa602fa
sithnuwanpramudith <sithnuwanpramudith2000@gmail.com>
2026-03-03 10:49:04 +0530

    අ

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const app = express();
-const port = 5000;
+//const port = 5000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -438,6 +438,7 @@ app.delete('/api/testimonials/:id', (req, res) => {
     });
 });
 
-app.listen(port, () => {
-    console.log(`Backend server running at http://localhost:${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("Server running");
 });
